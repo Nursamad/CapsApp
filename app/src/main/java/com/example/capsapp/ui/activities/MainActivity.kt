6d6bb4d6573261e9eb73f6d9b59369feb9d4da00
Nursamad <bagishbekow@gmail.com>
2022-02-04 17:57:00 +0600

    17:56

--- app/src/main/java/com/example/capsapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/capsapp/ui/activities/MainActivity.kt
@@ -1,11 +1,18 @@
-package com.example.capsapp
+package com.example.capsapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.capsapp.R
+import com.example.capsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.root
     }
 }
